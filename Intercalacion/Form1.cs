a49afc5498783f5b0143ffdc151179a132ac1110
pc <al23760375@ite.edu.mx>
2024-11-12 19:32:45 -0800

    - Cambios varios

--- Intercalacion/Form1.cs
+++ Intercalacion/Form1.cs
@@ -11,31 +11,24 @@ namespace Intercalacion
         {
             try
             {
-                // Crear instancia del intercalador
                 Intercalacion intercalador = new Intercalacion();
 
-                // Obtener los textos de los dos cuadros de texto
                 string texto1 = txtArchivo1.Text;
                 string texto2 = txtArchivo2.Text;
 
-                // Separar los números de cada cuadro de texto usando la coma como delimitador
                 string[] numeros1 = texto1.Split(',');
                 string[] numeros2 = texto2.Split(',');
 
-                // Convertir las cadenas a enteros, eliminando espacios innecesarios
                 var numerosConvertidos1 = numeros1.Select(num => Convert.ToInt32(num.Trim())).ToList();
                 var numerosConvertidos2 = numeros2.Select(num => Convert.ToInt32(num.Trim())).ToList();
 
-                // Realizar la intercalación de las dos listas
                 List<int> listaIntercalada = intercalador.IntercalarListas(numerosConvertidos1, numerosConvertidos2);
 
-                // Mostrar los números intercalados en el TextBox de resultado
                 txtResultado.Text = string.Join(", ", listaIntercalada);
             }
             catch (Exception ex)
             {
-                // Capturar cualquier excepción y mostrar un mensaje
-                MessageBox.Show($"Ocurrió un error: {ex.Message}");
+                MessageBox.Show($"Inserta un elemento válido");
             }
         }
     }
--- Intercalacion/Intercalacion.cs
+++ Intercalacion/Intercalacion.cs
@@ -8,13 +8,10 @@ namespace Intercalacion
 {
     public class Intercalacion
     {
-        // Función que toma dos listas ordenadas y las intercalan en una sola lista ordenada
         public List<int> IntercalarListas(List<int> lista1, List<int> lista2)
         {
             List<int> listaIntercalada = new List<int>();
             int i = 0, j = 0;
-
-            // Mientras haya elementos en ambas listas
             while (i < lista1.Count && j < lista2.Count)
             {
                 if (lista1[i] < lista2[j])
@@ -28,15 +25,11 @@ namespace Intercalacion
                     j++;
                 }
             }
-
-            // Si hay elementos restantes en lista1
             while (i < lista1.Count)
             {
                 listaIntercalada.Add(lista1[i]);
                 i++;
             }
-
-            // Si hay elementos restantes en lista2
             while (j < lista2.Count)
             {
                 listaIntercalada.Add(lista2[j]);
